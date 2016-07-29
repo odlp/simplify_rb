fb1d7b445d6fd0b489d8af358f4de20058f8feb0
odlp <oliverp@gmail.com>
2016-07-29 21:54:45 +0100

    Convert fixture data to yaml

--- spec/simplify_rb_spec.rb
+++ spec/simplify_rb_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 require 'simplify_rb'
-require File.expand_path('../fixtures/simplify_test_data.rb', __FILE__)
+require 'yaml'
 
 describe SimplifyRb::Simplifier do
   describe '#process' do
     context 'simplifies points correctly with the given tolerance' do
-      let(:test_data) { SimplifyTestData::points }
-      let(:expected_fast_result) { SimplifyTestData::result_fast }
-      let(:expected_high_quality_result) { SimplifyTestData::result_high_quality }
+      let(:test_data) { fixture_file('all-points.yml') }
+      let(:expected_result_fast) { fixture_file('result-fast.yml') }
+      let(:expected_result_high_quality) { fixture_file('result-high-quality.yml') }
 
-      it 'uses the fast strategy by default' do
+      it 'uses the fast strategy by default', focus: true do
         result = subject.process(test_data, 5)
-        expect(result).to eq(expected_fast_result)
+        expect(result).to eq(expected_result_fast)
       end
 
       it 'uses the high quality strategy when the flag is passed' do
         result = subject.process(test_data, 5, true)
-        expect(result).to eq(expected_high_quality_result)
+        expect(result).to eq(expected_result_high_quality)
       end
     end
 
@@ -39,5 +39,10 @@ describe SimplifyRb::Simplifier do
         expect { subject.process(data) }.to raise_error(ArgumentError, 'Points must be an array')
       end
     end
+  end
+
+  def fixture_file(name)
+    path = File.expand_path("../fixtures/#{name}", __FILE__)
+    YAML.load_file(path)
   end
 end
